c19b290da4fda7da289a9d582f95f11bc3507042
mahao2 <2727485259@qq.com>
2019-03-28 08:48:02 +0800

    提交

--- app/src/androidTest/java/cn/mh/class02/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cn/mh/class02/ExampleInstrumentedTest.java
@@ -1,9 +1,5 @@
 package cn.mh.class02;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,13 +10,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("cn.mh.class02", appContext.getPackageName());
-    }
+public class ExampleInstrumentedTest {
+
 }
